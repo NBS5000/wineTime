c79448173ee6c813390bf468034e30af0eef6f82
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-13 22:34:07 +1000

    grey added to behind video

--- client/src/components/Bg/index.js
+++ client/src/components/Bg/index.js
@@ -4,6 +4,7 @@ const bg = () => {
 
     return (
         <div id="bg">
+            <div id="bgTile"></div>
             <div id="bgOverlay"></div>
                 <video autoPlay muted loop id="bgVideo">
                     <source src="../../assets/video/winePourSlow.mp4" type="video/mp4" />
