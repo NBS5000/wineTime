2e836279a41be5c058d5e34e73e295ffc95c0bfc
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-17 20:47:52 +1000

    hopefully

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
 const Profile = require('./Profile');
 const Grape = require('./Grape');
-// const Collection = require('./Collection');
 const Blend = require('./Blend');
 const Style = require('./Style');
 const Wine = require('./Wine');
